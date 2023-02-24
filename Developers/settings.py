cf6f37b3270ea10ce284f277f50a14c9142ed766
Wamaitha <wamaithaweru19@gmail.com>
2023-02-24 23:39:04 +0300

    hide secret key in .env file

--- Developers/settings.py
+++ Developers/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa=+4k-+goi3l%(92ocqm2(^y0mmwa$s-*oyltecarl6!)b!es'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
